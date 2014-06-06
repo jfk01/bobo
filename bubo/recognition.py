c737810a0daf157958198e9ad0dd7bd0fe6e3294
jebyrne@gmail.com <jebyrne@gmail.com>
2014-06-06 17:09:34 -0400

    recognition app

---
+++ bubo/recognition.py
@@ -0,0 +1,8 @@
+# main entry point for spark based training and testing for a viset
+# this requires an algorithm that provides features
+# standard classification using linear svms
+# store results in cache
+
+# how do we learn new features
+
+
